ea2f6a696a1c5645971bad21d79e5fd9c28bb12f
ScriptedSage <jonathan.wirawan.zhang@gmail.com>
2024-12-08 12:57:32 +0700

    add-expo-camera

--- app/add-record/index.tsx
+++ app/add-record/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Pressable, Text, View } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
 
 const AddRecordScreen = () => {
+  const [facing, setFacing] = useState<CameraType>("front");
+  const [permission, requestPermission] = useCameraPermissions();
+  const camera = useRef(null);
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <View style={{ padding: 8 }}>
       <View
         style={{
+          overflow: "hidden",
           width: "100%",
           backgroundColor: "lightgray",
           aspectRatio: "1 / 1",
@@ -15,7 +24,16 @@ const AddRecordScreen = () => {
           borderRadius: 8,
         }}
       >
-        <Feather name="camera" size={64} color="rgba(0, 0, 0, 0.8)" />
+        {permission?.granted ? (
+          <CameraView
+            ref={camera}
+            style={{ aspectRatio: "1 / 1" }}
+            facing={facing}
+            ratio="1:1"
+          ></CameraView>
+        ) : (
+          <Feather name="camera" size={64} color="rgba(0, 0, 0, 0.8)" />
+        )}
       </View>
       <Pressable
         style={{
