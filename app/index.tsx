6d0b0714c0162d17394c7cf6d1ab2d5adb87ed5e
leptonite-dev <azmiarzaki@gmail.com>
2024-12-19 17:53:21 +0700

    change storage to sqlite

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
 import { IconButton } from "@/components/IconButton";
 import { useFonts } from "expo-font";
-import React from "react";
+import React, { useEffect } from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { ScrollView, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Link, useNavigation, useRouter } from "expo-router";
+
 const mythings = [{ name: "kunci" }, { name: "ball" }];
+
 const index = () => {
   const [loaded] = useFonts({
     Mulish: require("../assets/fonts/Mulish-VariableFont_wght.ttf"),
   });
+  const router = useRouter();
+
   return (
     <View>
       <ScrollView>
@@ -39,6 +43,21 @@ const index = () => {
 
       <Link
         href="/add-record"
+        style={{
+          alignSelf: "center",
+        }}
+      >
+        <IconButton
+          style={{
+            borderRadius: 8,
+            backgroundColor: "#966231",
+            padding: 8,
+          }}
+          icon={<AntDesign name="plus" size={24} color="white" />}
+        />
+      </Link>
+      <Link
+        href="/storage-test"
         style={{
           alignSelf: "center",
         }}
--- app/storage-test/index.tsx
+++ app/storage-test/index.tsx
@@ -1,31 +1,65 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import * as SQLite from "expo-sqlite";
+
 interface TRecord {
   name: string;
 }
+
 const index = () => {
+  const [db, setDB] = useState<SQLite.SQLiteDatabase>();
+
   const storeData = async (value: TRecord) => {
     try {
-      const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem(Date.now().toString(), jsonValue);
     } catch (e) {
-      // saving error
-    }
-  };
-  const getData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getAllKeys()
-      console.log(jsonValue)
-    } catch (e) {
-      // error reading value
+      console.log(e);
     }
   };
 
+  const getAllData = async () => {
+    if (!db) return;
+
+    try {
+      const allRows = await db.getAllAsync<{
+        id: number;
+        name: string;
+        location: string;
+      }>("SELECT * FROM records");
+      for (const row of allRows) {
+        console.log(row);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const createTable = async (db: SQLite.SQLiteDatabase) => {
+    try {
+      await db.execAsync(`
+        PRAGMA journal_mode = WAL;
+        CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, location TEXT NOT NULL);
+        `);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const initDB = async () => {
+    const db = await SQLite.openDatabaseAsync("my-things");
+    console.log(db)
+    createTable(db);
+
+    setDB(db);
+  };
+
+  useEffect(() => {
+    initDB();
+  }, []);
+
   return (
     <View>
-      <Button onPress={()=>{storeData({name:"jam"})}} title="add"></Button>
-      <Button  title="get" onPress={getData}></Button>
+      <Button onPress={() => {}} title="add"></Button>
+      <Button title="get" onPress={getAllData}></Button>
       <Text>index</Text>
     </View>
   );
